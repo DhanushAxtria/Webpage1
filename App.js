c19a22d3520a4bfe57a330f2ec0c52f2c750ed3a
DhanushAxtria <saidhanush95.gudipati@gmail.com>
2024-10-08 14:40:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Body from './Body/Body';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
